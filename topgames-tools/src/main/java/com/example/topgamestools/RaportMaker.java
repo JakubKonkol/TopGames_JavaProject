d28216ee5ab4e65e282ea01d65fbd5ae3359b28a
JakubKonkol <s24406@pjwstk.edu.pl>
2023-01-22 13:29:17 +0100

    ew

---
+++ topgames-tools/src/main/java/com/example/topgamestools/RaportMaker.java
@@ -0,0 +1,15 @@
+package com.example.topgamestools;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RaportMaker {
+    public List<String> raports = new ArrayList<>();
+
+    public RaportMaker() {
+
+    }
+    public void addRaport(String raport){
+        raports.add(raport);
+    }
+}
