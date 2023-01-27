0b2c310c8fdc67cc1f2289e1a2e15bbec5d45997
JakubKonkol <s24406@pjwstk.edu.pl>
2023-01-27 19:26:44 +0100

    finished

---
+++ README.md
@@ -0,0 +1,11 @@
+# TopGames
+### Autor: Jakub Konkol s24406
+### Ocena: 5.0
+
+- Spring REST Api ✅
+- Warstwa bazodanowa oparta o repozytoria Spring ✅
+- Rest Controllery dla zasobów bazodanowych ✅
+- projekt wielomodułowy ✅
+- wykorzystanie logowania (np. Log4J lub SL4J) + serwis do odczytywania logów ✅
+- wykorzystanie Spring Security + OAuth (dodatkowe 0.5) ✅
+- Frontend: Angular✅
--- topgames-web-api/src/main/java/com/example/topgameswebapi/configuration/SecurityConfiguration.java
+++ topgames-web-api/src/main/java/com/example/topgameswebapi/configuration/SecurityConfiguration.java
@@ -4,32 +4,37 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
+
+import static com.fasterxml.jackson.databind.cfg.CoercionInputShape.Array;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 
 @Configuration
 public class SecurityConfiguration{
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeHttpRequests()
-//                .requestMatchers("/api/**", "/logs/**", "/logs", "/api")
-//                .permitAll()
-//                .and()
-//                .authorizeHttpRequests()
-//                .anyRequest().authenticated();
-//
-//        http
-//                .oauth2Login();
-//        return http.build();
-//    }
     @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().requestMatchers("/api/**", "/logs/**");
-    }
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests()
+                .requestMatchers("/api/**", "/logs/**", "/logs", "/api", "/oauth2/**", "/oauth2")
+                .permitAll()
+                .and()
+                .authorizeHttpRequests()
+                .anyRequest().authenticated()
+                .and()
+                .cors().disable()
+                .csrf().disable();
 
+        http
+                .oauth2Login();
+        return http.build();
+    }
 
 }
 
